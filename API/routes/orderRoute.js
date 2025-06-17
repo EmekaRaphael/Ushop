cfbad780a206170e27a1e41565d9e8cb152c631c
EmekaRaphael <emekaraphael13@gmail.com>
2025-06-17 17:44:56 +0100

    updated order widget for admin backend

--- API/routes/orderRoute.js
+++ API/routes/orderRoute.js
@@ -60,7 +60,7 @@ router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
 //GET ALL ORDERS
 router.get("/", verifyTokenAndAdmin, async (req, res) => {
     try {
-        const orders = Order.find();
+        const orders = await Order.find();
         res.status(200).json(orders);
     } catch (err) {
         res.status(500).json(err);
